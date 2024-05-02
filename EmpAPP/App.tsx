2a8f7aaca6ca4c46af01fced041d1e36e30d7711
harshad.sonawane <sonawaneharshad1999@gmail.com>
2024-05-02 15:54:49 +0530

    init: added Navigation configurations and added vector icons

--- EmpAPP/App.tsx
+++ EmpAPP/App.tsx
@@ -1,6 +1,7 @@
 import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   useEffect(() => {
@@ -12,6 +13,7 @@ const App = () => {
   return (
     <View>
       <Text>App</Text>
+      <FontAwesome name="home" size={50} color="#eee" />
     </View>
   );
 };
